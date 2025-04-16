060b901653bf8459495369f2f0ca397fbb844946
aleguy02 <alejandro.the.villate@gmail.com>
2025-04-15 22:49:24 -0400

    Create Spotify login route

--- app.py
+++ app.py
@@ -1,11 +1,48 @@
-from flask import Flask
+import os
+from dotenv import load_dotenv
+from flask import Flask, request, redirect
+from urllib.parse import urlencode
+
+from src.utils import generate_random_string
+
+load_dotenv()
+
+client_id: str = os.getenv("CLIENT_ID")
+redirect_uri: str = "http://127.0.0.1:5000/callback"
 
 app = Flask(__name__)
 
+# redirect depending on sign-in state
 @app.route("/")
-def hello_world():
+def redir():
     return "<p>Hello, World!</p>"
+
+@app.route("/login")
+def login_to_spotify():
+    # For securing our request/response
+    state: str = generate_random_string(10)
+
+    # Spotify permissions scopes
+    scope: str = "user-top-read"
+
+    query_params: dict = {
+        "response_type": "code",
+        "client_id": client_id,
+        "scope": scope,
+        "redirect_uri": redirect_uri,
+        "state": state,
+    }
+
+    auth_url: str = "https://accounts.spotify.com/authorize?" + urlencode(query_params)
+    return redirect(auth_url)
+
+
+# @app.route("/haiku")
+# def generate_haiku():
+#     pass
+
 
 
 if __name__ == "__main__":
+
     app.run(host="0.0.0.0", port=5000)
---
+++ src/utils.py
@@ -0,0 +1,15 @@
+import secrets
+import string
+
+# credit to https://www.reddit.com/r/pythontips/comments/1fzxjv3/how_to_generate_random_strings_in_python/
+def generate_random_string(length: int) -> str:
+    """
+    Generate a random alphanumeric string of the specified length.
+    This function creates a string consisting of randomly selected 
+    uppercase letters, lowercase letters, and digits.
+    Args:
+        length (int): The length of the random string to generate.
+    Returns:
+        str: A randomly generated alphanumeric string of the specified length.
+    """
+    return ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(length))
